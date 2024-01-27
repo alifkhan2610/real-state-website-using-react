7c559816f79fc6912c9e4698c8146c5897811cce
alifkhan2020 <alifkhan.bubt@gmail.com>
2024-01-27 23:35:57 +0600

    3rd Commit

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import React from "react";
 import {
   Accordion,
@@ -37,14 +38,20 @@ const Value = () => {
             preExpanded={[0]}
           >
             {data.map((item, i) => {
+              const [className, setClassName] = useState(null)
               return (
-                <AccordionItem className="accordionItem" key={i} uuid={i}>
+                <AccordionItem className={`accordionItem ${className}`} key={i} uuid={i}>
                   <AccordionItemHeading>
-                    <AccordionItemButton>
+                    <AccordionItemButton className="flexCenter accordionButton">
+                      <AccordionItemState>
+                        {
+                          ({expanded}) => expanded ? setClassName("expanded") : setClassName("collapsed")
+                        }
+                      </AccordionItemState>
                       <div className="flexCenter icon">
-                        {AccordionItem.icon}
+                        {item.icon}
                       </div>
-                      <span className="">{item.heading}</span>
+                      <span className="primaryText">{item.heading}</span>
                       <div className="flexCenter icon">
                         <MdOutlineArrowDropDown size={20} />
                       </div>
