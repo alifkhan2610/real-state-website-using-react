6e0b2704a952d85419236d779a49eab195a69d89
MD ALIF <alifkhan.bubt@gmail.com>
2024-01-29 17:41:46 +0600

    Site is done without responsive design.

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get Started With Homzy</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quatation
+            <br />
+            Find your residence soon
+          </span>
+          <div className="button">
+            <a href="mailto:alifkhan.bubt@gmail.com">Get Started</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetStarted;
